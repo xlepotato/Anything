1a43ee5abe1cf17f94788bd4c85f6db656866060
xiaochaorenn <54619857+xiaochaorenn@users.noreply.github.com>
2019-10-16 20:39:53 +0800

    update
    merge header with stylesheet css
    comment api

--- Anything/Controllers/HomeController.cs
+++ Anything/Controllers/HomeController.cs
@@ -15,25 +15,25 @@ namespace Anything.Controllers
     {  
         public async System.Threading.Tasks.Task<ActionResult> Index()
         {
-            await ApiController.SetRates();
+            //await ApiController.SetRates();
             using (cz2006anythingEntities model = new cz2006anythingEntities())
             {
                 return View(model.Currencies.ToList());
             }
         }
-        public ActionResult GetCurrency(float ExchangeAmount, string ExchangeFrom, string ExchangeTo)
-        {
-            return Json(ApiController.GetCurrency(ExchangeAmount,ExchangeFrom,ExchangeTo), JsonRequestBehavior.AllowGet);
-        }
+        //public ActionResult GetCurrency(float ExchangeAmount, string ExchangeFrom, string ExchangeTo)
+        //{
+        //    return Json(ApiController.GetCurrency(ExchangeAmount,ExchangeFrom,ExchangeTo), JsonRequestBehavior.AllowGet);
+        //}
 
         public ActionResult Filter(string Search,string ExchangeFrom, string ExchangeTo)
         {
             return Json(FilterController.Filter(Search,ExchangeFrom,ExchangeTo), JsonRequestBehavior.AllowGet);          
         }
-        public async System.Threading.Tasks.Task<ActionResult> GetGraph(string ExchangeFrom, string ExchangeTo)
-        {
-            return Json(await GraphController.GetGraph(ExchangeFrom,ExchangeTo), JsonRequestBehavior.AllowGet);
-        }              
+        //public async System.Threading.Tasks.Task<ActionResult> GetGraph(string ExchangeFrom, string ExchangeTo)
+        //{
+        //    return Json(await GraphController.GetGraph(ExchangeFrom,ExchangeTo), JsonRequestBehavior.AllowGet);
+        //}              
     }
 }
   
